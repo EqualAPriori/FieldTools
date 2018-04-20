e834d3676b89e45fea76743973668a2d072054af
Joshua Lequieu <lequieu@mrl.ucsb.edu>
2018-04-20 12:39:39 -0700

    Correctly identify domains, mesh them, and also calculate their area and volume

--- traj-analysis/domaintools.py
+++ traj-analysis/domaintools.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from skimage import measure
 import pdb
 
 '''
@@ -10,16 +11,22 @@ import pdb
         Based off of OperatorBridging in Josh's PolyFTS bridging code
 
     - mesh using marching cubes
-    - calculate volume and area of domains
+    - calculate volume and area of domains using mesh
 
 '''
+
+# for burning algorithm
 import sys
 sys.setrecursionlimit(100000)
 
 class DomainAnalyzer:
-    def __init__(self,coords,fields):
+    def __init__(self,coords,fields, density_field_index=0):
+        ''' Define and calculate useful variables for DomainAnalysis routines
+        '''
+
         self.__coords = coords
         self.__fields = fields
+        self.__density_field_index = density_field_index
         self.__ndim = len(coords.shape) - 1
         self.__Nx = coords.shape[:3]
         self.__nfields = len(fields.shape) - self.__ndim
@@ -29,72 +36,164 @@ class DomainAnalyzer:
             raise ValueError("coords[0,0,0] != (0,0,0)")
         self.__boxl = tuple(self.__coords[-1,-1,-1]) 
         self.__boxh = tuple(self.__coords[-1,-1,-1]*0.5) 
-
+        self.__gridspacing = (self.__coords[1,0,0][0], self.__coords[0,1,0][1], self.__coords[0,0,1][2])
+   
+    def getDomainStats(self,getCenter=True, getArea=True, getVol=True, getIQ=True, plotMesh=False):
 
         density_threshold = 0.5
         # create boolean selector from density fields for region definition
-        isdomain_array = (fields[:,:,:,0] > density_threshold)
+        isdomain_array = (self.__fields[:,:,:,self.__density_field_index] > density_threshold)
 
         # FIXME, things break for non-cubic boxes. It must have to do with the vtk vs numpy indexing
 
-        import PolyFTS_to_VTK
-        AllCoords = np.reshape(coords,(self.__M, self.__ndim))
-        AllCoords = AllCoords.T
-
         # identify domains
         self.__regionID = None # initially empty, created in computeRegionIDs
-        self.__nregions = self.identifyAndIndexDomains(isdomain_array)
+        self.__ndomains = self.identifyAndIndexDomains(isdomain_array)
+
+        # plotting for debugging
+        #import PolyFTS_to_VTK
+        #AllCoords = np.reshape(coords,(self.__M, self.__ndim))
+        #AllCoords = AllCoords.T
+
+        ##tmp = np.ravel(self.__image_flags)
+        ##tmp = np.resize(tmp,(len(tmp),3))
+        ##tmp = tmp.T
+        ##PolyFTS_to_VTK.writeVTK("image_flags.vtk", self.__Nx, True, self.__M, AllCoords, tmp)
+        #tmp = np.ravel(self.__borderID)
+        #tmp = np.resize(tmp,(1,len(tmp)))
+        #PolyFTS_to_VTK.writeVTK("borderID.vtk", self.__Nx, True, self.__M, AllCoords,tmp)
+
+        #domain_ids = np.ravel(self.__regionID)
+        #domain_ids = np.resize(domain_ids,(1,len(domain_ids)))
+        #PolyFTS_to_VTK.writeVTK("domains.vtk", self.__Nx, True, self.__M, AllCoords, domain_ids)
         
+        nstats = 1+ 3*getCenter + getArea + getVol + getIQ
+        stats = np.zeros((self.__ndomains,nstats))
 
-        #tmp = np.ravel(self.__image_flags)
-        #tmp = np.resize(tmp,(len(tmp),3))
-        #tmp = tmp.T
-        #PolyFTS_to_VTK.writeVTK("image_flags.vtk", self.__Nx, True, self.__M, AllCoords, tmp)
+        #for each domain
+        for idomain in range(1,self.__ndomains+1):
+            icol = 0 
+            stats[idomain-1,icol] = idomain; icol += 1
 
+            # calc center of domain
+            if getCenter:
+                com = self.calcDomainCOM(idomain,units='coord')
+                for i in range(self.__ndim):
+                    stats[idomain-1,icol] = com[i]; icol += 1
+            
+            if getArea or getVol or getIQ:
+                # mesh domain
+                verts, faces, normals, values = self.meshDomain(idomain, density_threshold)
 
-        
-        
-        tmp = np.ravel(self.__isborder)
-        tmp = np.resize(tmp,(1,len(tmp)))
-        PolyFTS_to_VTK.writeVTK("isborder.vtk", self.__Nx, True, self.__M, AllCoords,tmp)
+                # get surface area, volume and isoperimetric quotient
+                if getArea or getIQ:
+                    surface_area = measure.mesh_surface_area(verts, faces)
+                    if getArea:
+                        stats[idomain-1,icol] = surface_area; icol += 1
+                if getVol or getIQ:
+                    volume = self.mesh_volume(verts,faces)
+                    if getVol:
+                        stats[idomain-1,icol] = volume; icol += 1
+                if getIQ:
+                    IQ = self.calcIQ(surface_area, volume)
+                    stats[idomain-1,icol] = IQ; icol += 1
+            
+            if plotMesh: 
+                self.plotMesh(verts,faces, "mesh.{}.png".format(idomain))
 
-        domain_ids = np.ravel(self.__regionID)
-        domain_ids = np.resize(domain_ids,(1,len(domain_ids)))
-        PolyFTS_to_VTK.writeVTK("domains.vtk", self.__Nx, True, self.__M, AllCoords, domain_ids)
-
-        pdb.set_trace()
+        return stats
     
-        self.calcDomainCOM(1)
-        self.encloseDomainInBox(1)
+    def calcIQ(self, area, vol):
+        return 36.0*np.pi * vol*vol / (area * area * area)
 
+    def meshDomain(self,idomain, density_threshold):
+        '''
+        Function to:
+        1) apply PBC to the domains so that an entire domain is continuous (ie not split across boundaries)
+        2) Grab a little margin around each domain (the domain's "border") so that marching cubes can interpolate. The border is computed in identifyAndIndexDomains().
+        3) Mesh the domain using marching cubes
+        '''
+        
+        isdomain = (self.__regionID == idomain)
+        isborder = (self.__borderID == idomain)
+
+        com = self.calcDomainCOM(idomain,units='box')
+        
+        # center box and properties around center of mass (so that domains don't cross pbc)
+        # np.roll is the key function here
+        alldensity = self.__fields[:,:,:, self.__density_field_index]
+        #coords_tmp = np.copy(self.__coords)
+        for i in range(self.__ndim):
+            shift = int(0.5*self.__Nx[i] - com[i])
+            isdomain = np.roll(isdomain,shift,axis=i)
+            isborder = np.roll(isborder,shift,axis=i)
+            #coords_tmp = np.roll(coords_tmp,shift,axis=i)
+            alldensity = np.roll(alldensity,shift,axis=i)
+
+               
+        # isolate the domain of interest
+        isdomain_or_isborder = isdomain + isborder # since both bool, sum is the union of the two fields
+        mydensity = np.zeros(self.__Nx)
+        mydensity[isdomain_or_isborder] = alldensity[isdomain_or_isborder]
+
+        # plot for debugging
+        #import PolyFTS_to_VTK
+        #AllCoords = np.reshape(coords_tmp,(self.__M, self.__ndim))
+        #AllCoords = AllCoords.T
+        #tmp = np.ravel(isdomain)
+        #tmp = np.resize(tmp,(1,len(tmp)))
+        #PolyFTS_to_VTK.writeVTK("isdomain.vtk", self.__Nx, True, self.__M, AllCoords,tmp)
+        #tmp = np.ravel(isborder)
+        #tmp = np.resize(tmp,(1,len(tmp)))
+        #PolyFTS_to_VTK.writeVTK("isborder.vtk", self.__Nx, True, self.__M, AllCoords,tmp)
+        #tmp = np.ravel(mydensity)
+        #tmp = np.resize(tmp,(1,len(tmp)))
+        #PolyFTS_to_VTK.writeVTK("mydensity.vtk", self.__Nx, True, self.__M, AllCoords,tmp)
+
+        if self.__ndim != 3:
+            raise NotImplementedError("Due to use of Scikit-image's marching cubes. Meshing\
+                currently only works in 3 dimensions. Will need to use a different library \
+                to mesh in 2d")
+    
+        # mesh! (using scikit-image)
         #from skimage import measure
-        #verts, faces, normals, values = measure.marching_cubes_lewiner(isdomain_array, 0)
+        verts, faces, normals, values = measure.marching_cubes_lewiner(mydensity, density_threshold, spacing = self.__gridspacing)
 
-        #import matplotlib.pyplot as plt
-        #from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-        #fig = plt.figure(figsize=(10, 10))
-        #ax = fig.add_subplot(111, projection='3d')
+        return verts, faces, normals, values
 
-        ### Fancy indexing: `verts[faces]` to generate a collection of triangles
-        #mesh = Poly3DCollection(verts[faces])
-        #mesh.set_edgecolor('k')
-        #ax.add_collection3d(mesh)
+    def mesh_volume(self, verts, faces):
+        actual_verts = verts[faces]
+        v0 = actual_verts[:,0,:]
+        v1 = actual_verts[:,1,:]
+        v2 = actual_verts[:,2,:]
+        # 1/6 \sum v0 \cdot (v1 x v2)
+        return 1.0/6.0 * np.abs( (v0*np.cross(v1,v2)).sum(axis=1).sum() )
+ 
+    def plotMesh(self, verts, faces, filename=None):
+        ''' Plot a mesh from marching cubes
+        '''
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+        # Display resulting triangular mesh using Matplotlib. This can also be done
+        # with mayavi (see skimage.measure.marching_cubes_lewiner docstring).
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(111, projection='3d')
 
-        #ax.set_xlim(0, 32)  # a = 6 (times two for 2nd ellipsoid)
-        #ax.set_ylim(0, 32)  # b = 10
-        #ax.set_zlim(0, 64)  # c = 16
+        # Fancy indexing: `verts[faces]` to generate a collection of triangles
+        mesh = Poly3DCollection(verts[faces])
+        mesh.set_edgecolor('k')
+        ax.add_collection3d(mesh)
 
-        #plt.tight_layout()
-        #plt.show()
+        ax.set_xlim(0, self.__boxl[0])  
+        ax.set_ylim(0, self.__boxl[1])  
+        ax.set_zlim(0, self.__boxl[2])  
 
+        plt.tight_layout()
+        if not filename:
+            plt.show()
+        else:
+            plt.savefig(filename)
 
-
-
-
-    def getRegionIDs(self):
-        return self.__regionID, self.__numRegionNeighbors, self.__nregions
-
-    
     def calcDomainCOM(self,idomain, units='box'):
         ''' given a domain index, apply PBC and return the center of mass
             Can return result in 'box' units (0 to Nx) or in 'coord' units (0 to boxl)
@@ -103,9 +202,10 @@ class DomainAnalyzer:
         isdomain = (self.__regionID == idomain)
         N = np.sum(isdomain)
         indicies = np.transpose(np.nonzero(isdomain))
+        
+        
         coords = np.zeros((N,self.__ndim))
 
-        
         #TODO could I do this without for loop? (will be faster)
         for i in range(N):
             index = tuple(indicies[i])
@@ -122,30 +222,6 @@ class DomainAnalyzer:
         # now average in order to get center of the domain (each point weighted evenly)
         return np.average(coords,axis=0)
 
-    def meshDomain(self,idomain):
-        '''
-        Function to:
-        1) apply PBC to the domains so that an entire domain is continuous (ie not split across boundaries)
-        2) Grab a little margin around each domain so that marching cubes can interpolate
-        3) Mesh the domain using marching cubes
-        '''
-        
-
-        com = calcDomainCOM(idomain,units='box')
-
-        # get 
-
-
-
-    def encloseDomainInBox(self,idomain):
-        ''' 
-        determine minimal orthorhombic box that encloses a domain 
-        return value: a boolean mask of shape self.__Nx
-        '''
-
-        is_target_domain = (self.__regionID == idomain)
-
-
 
     def identifyAndIndexDomains(self, isdomain_array):
         ''' This function populates the regionID member variable
@@ -157,7 +233,7 @@ class DomainAnalyzer:
         # if regionID == -1, it has not been visited
         #M = np.size(isdomain_array)
         self.__regionID = np.full(self.__Nx,-1,dtype=np.int32);
-        self.__isborder = np.full(self.__Nx,False,dtype=np.bool);
+        self.__borderID = np.full(self.__Nx,0,dtype=np.int32);
         self.__image_flags = np.zeros(list(self.__Nx) + [self.__ndim])
         
 
@@ -171,14 +247,9 @@ class DomainAnalyzer:
               self.spread_region(i, region_number, isdomain_array,current_image_flag);
               region_number += 1;
             else:
+              # note - dont assign borders here, this is acomplished inside of spread_region()
               self.__regionID[i] = 0;
               self.__image_flags[i]= np.zeros(self.__ndim)
-
-              # get neighbors to determine if this is a border
-              neighbors, neigh_image_flags = self.getNeighbors(i)
-              nneigh_in_domain = sum([isdomain_array[n] for n in neighbors])
-              if nneigh_in_domain >= 1:
-                  self.__isborder[i] = True
 
         nregions = region_number-1;
 
@@ -204,12 +275,15 @@ class DomainAnalyzer:
                   self.spread_region(neighbor, region_number, isdomain_array, image_flag);
                 else:
                   self.__regionID[neighbor] = 0;
-                  # must have neighbors that are domain (since spread region is only called 
-                  #   if coord_center is a domain). Therefore, it's a border
-                  self.__isborder[neighbor] = True
                   
 
             if self.__regionID[neighbor] == 0:
+              # must have neighbors that are domain (since spread region is only called 
+              #   if coord_center is a domain). Therefore, it's a border
+              if self.__borderID[neighbor] != 0 and self.__borderID[neighbor] != region_number:
+                raise RuntimeError("Trying to set borderID[{0}]={1} but is it is already set to {2}".format(neighbor, region_number, self.__borderID[neighbor]))
+              self.__borderID[neighbor] = region_number
+ 
               # set image flags of non-domain adjacent to domain according to the domain
               # basically, I need the border to have the correct image flags
 
@@ -218,10 +292,12 @@ class DomainAnalyzer:
               #   Raise and error just in case...
               #   make sure, not (0,0,0) and not new == old
               if not np.all(self.__image_flags[neighbor]==0) and not np.all(self.__image_flags[neighbor] == image_flag):
-                raise RuntimeError("Tried to set image_flag of {0} to {2} but it was already set to {1} and not (0,0,0)".format(neighbor,self.__image_flags[neighbor], image_flag))
+                raise RuntimeError("Tried to set image_flag of {0} to {2} but it was already set \
+                    to {1} and not (0,0,0)".format(neighbor,self.__image_flags[neighbor], image_flag))
               
               self.__image_flags[neighbor] = image_flag
-        
+
+       
     def getNeighbors(self,coord_center,center_image_flag=[]):
        ''' given a coord (tuple), return 
             1) the neighbors of that coord (also tuple) AND 
@@ -256,7 +332,3 @@ class DomainAnalyzer:
              coord[i] = self.__Nx[i] - 1
              image_flag[i] -= 1
             
-
-# testing code if executed directly
-if __name__ == "__main__":
-    pass
