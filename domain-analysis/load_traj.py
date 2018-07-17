7c4bd48d6bfd3e03d2820394c8dc5c336e95fcc8
Joshua Lequieu <lequieu@mrl.ucsb.edu>
2018-07-17 09:21:54 -0700

    added command line args and ability to save and load info from frames - this allows you to only compute domains once, and then quickly calculate other properties afterwards from the saved stats files

--- domain-analysis/load_traj.py
+++ domain-analysis/load_traj.py
@@ -43,15 +43,29 @@ def mapDomainIndicies(comA, comB):
 
 if __name__ == "__main__":
     
-    #import argparse as ap
-    #parser = ap.ArgumentParser(description='Analyze a Trajectory')
-    #parser.add_argument('infiles',metavar='inputfiles',nargs='*',default=['./density.bin'],help='Input filename containing unformatted Field data')
-    #parser.add_argument('-v','--verbose',action='store_true',default=False,help='Print lots of extra info')
-    ## Parse the command-line arguments
-    #args=parser.parse_args()
+    import argparse as ap
+    parser = ap.ArgumentParser(description='Analyze a Trajectory')
+    parser.add_argument('--densityfiles',metavar='inputfiles',nargs='+',default=glob.glob('density*bin'),help='Input filename containing unformatted Field data')
+    parser.add_argument('-n','--navg',default=100,type=int,help='Number of frames to average the fields over before computing domains')
+    parser.add_argument('-s','--save',action='store_true',help='Input filename containing unformatted Field data')
+    parser.add_argument('-l','--load',action='store_true',help='Input filename containing unformatted Field data')
+    parser.add_argument('--novtk',action='store_true',help='flag to turn off saving of VTK files of averaged densities')
+    # Parse the command-line arguments
+    args=parser.parse_args()
 
     #infiles = args.infiles
-    infiles = glob.glob('density*bin')
+    saveflag = args.save
+    loadflag = args.load
+    novtkflag = args.novtk
+    if (saveflag == True) and (loadflag == True):
+        raise ValueError ("Flags --save and --load cannot both be specified")
+    elif (saveflag == False) and (loadflag == False):
+        # save flag defaults to true if neither loading or saving is set
+        saveflag = True
+    
+    nblocksize = args.navg #100 # number of frames to average together
+
+    infiles = args.densityfiles #glob.glob('density*bin')
     # parser
     infiles.sort(key=natural_keys)
     print(infiles)
@@ -63,7 +77,6 @@ if __name__ == "__main__":
         ndigit=sum( c.isdigit() for c in infile)  
         if ndigit > maxdigit: maxdigit = ndigit
     
-    nblocksize = 20 # number of frames to average together before
     nframes = len(infiles)
     nblocks = int(nframes / nblocksize)
     iblock = 0
@@ -71,45 +84,74 @@ if __name__ == "__main__":
 
     count = 0
     for iframe, infile in enumerate(infiles):
-        print("Analyzing {}".format(infile))
+        
+        if saveflag:
+            print("Analyzing {}".format(infile))
+            coords, fields = io.ReadBinFile(infile)
 
-        coords, fields = io.ReadBinFile(infile)
-
-        if iframe == 0:
-            sumcoords = np.copy(coords)
-            sumfields = np.copy(fields)
-        else:
-            sumcoords = sumcoords + coords
-            sumfields = sumfields + fields
-        count +=1
+            if iframe == 0:
+                sumcoords = np.copy(coords)
+                sumfields = np.copy(fields)
+            else:
+                sumcoords = sumcoords + coords
+                sumfields = sumfields + fields
+            count +=1
 
         if (iframe+1) % nblocksize == 0:
-            sumcoords /= count
-            sumfields /= count
+            if saveflag:
+                sumcoords /= count
+                sumfields /= count
+                
+                if not novtkflag:
+                    viz.writeVTK("block{}.vtk".format(iblock), sumcoords, sumfields)
 
-            viz.writeVTK("block{}.vtk".format(iblock), sumcoords, sumfields)
+                domainanalyzer = DomainAnalyzer(sumcoords,sumfields)
+                ndomains[iblock], com, a, v, IQ = domainanalyzer.getDomainStats(plotMesh=False,useMesh=False)
+                #ndomains[iblock], com, a, v, IQ = domainanalyzer.getDomainStats(plotMesh=True)
 
-            domainanalyzer = DomainAnalyzer(sumcoords,sumfields)
-            ndomains[iblock], com, a, v, IQ = domainanalyzer.getDomainStats(plotMesh=True)
-            if iblock == 0:
-                area = np.zeros((nblocks,ndomains[iblock]+5))
-                vol = np.zeros((nblocks,ndomains[iblock]+5))
+                if (ndomains[iblock] != ndomains[iblock-1]):
+                   print("Warning: Number of domains not constant throughout trajectory {} {}".format(ndomains[iblock],ndomains[iblock-1]))
 
-            if iblock == 0:
-               area[iblock][0:len(a)] = a 
-               vol[iblock][0:len(v)] = v
-            elif iblock and (ndomains[iblock] == ndomains[iblock-1]):
-               area[iblock][0:len(a)] = a 
-               vol[iblock][0:len(v)] = v
-            else:
-                print("Warning: Number of domains not constant throughout trajectory {} {}".format(ndomains[iblock],ndomains[iblock-1]))
+                stats = np.vstack((com.T,a,v,IQ)).T
+                np.savetxt("stats{}.dat".format(iblock),stats, header="comx comy comz area vol IQ")
+            
+                #np.savetxt("com{}.dat".format(iblock),com)
 
-            sumcoords = np.zeros(sumcoords.shape)
-            sumfields = np.zeros(sumfields.shape)
-            count = 0
+                # ---------------------
+                # Eventually I'd like to remove this code but I'll leave it for now for legacy reasons
+                if iblock == 0:
+                    area = np.zeros((nblocks,ndomains[iblock]+20))
+                    vol = np.zeros((nblocks,ndomains[iblock]+20))
+
+                area[iblock][0:len(a)] = a 
+                vol[iblock][0:len(v)] = v
+
+                np.savetxt("ndomains.dat",ndomains)
+                np.savetxt("area.dat",area)
+                np.savetxt("vol.dat",vol)
+
+                # ---------------------
+
+                # zero quantities
+                count = 0
+                sumcoords = np.zeros(sumcoords.shape)
+                sumfields = np.zeros(sumfields.shape)
+
+            elif loadflag:
+                print("Loading stats{}.dat".format(iblock))
+                stats=np.loadtxt("stats{}.dat".format(iblock))
+                com = stats[:,0:3]
+                a = stats[:,3]
+                v  = stats[:,4]
+                IQ = stats[:,5]
+
+
+            
+            # here com, area, vol, IQ were either computed, or loaded
+            #pdb.set_trace() #check that the variables are reasonable
+
+
+            # update block 
             iblock +=1
         
-            np.savetxt("ndomains.dat",ndomains)
-            np.savetxt("area.dat",area)
-            np.savetxt("vol.dat",vol)
 
