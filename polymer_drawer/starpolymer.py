26fbba6db0cd1d2d882259ce3baa1f802cea0207
Trenton Koeper <tmkoeper@braid.cnsi.ucsb.edu>
2018-08-20 15:52:41 -0700

    Fixed case where only one polymer is drawn

--- polymer_drawer/starpolymer.py
+++ polymer_drawer/starpolymer.py
@@ -3,12 +3,14 @@ import numpy as np
 import pdb
 import matplotlib.pyplot as plt
 import argparse,os,sys,glob,re
+import pickle
 pi = np.pi
 
 
 
 class star():
     def __init__(self,filename,cwd):
+        self.lines = []
         with open(filename,"r") as f:
             isstar = False
             self.armlist = []
@@ -46,12 +48,88 @@ class star():
             #number of diblock arms for graphing with respect to narms and phi
             self.n_diblock=self.armlist[1].multiplicity
             #initilize the species amounts to all 0
-            species_amounts = [0]*(max([max(a.species) for a in self.armlist])+1)
-            for myarm in self.armlist:
-                for spec,bf in zip(myarm.species,myarm.block_frac):
-                    species_amounts[spec] = bf*myarm.length
-            phiA = species_amounts[0]/sum(species_amounts)
-            self.phi = round(phiA,2)#round for aesthetics
+           # species_amounts = [0]*(max([max(a.species) for a in self.armlist])+1)
+           # for myarm in self.armlist:
+           #     for spec,bf in zip(myarm.species,myarm.block_frac):
+           #         species_amounts[spec] = bf*myarm.length
+           # phiA = species_amounts[0]/sum(species_amounts)
+           # self.phi = round(phiA,2)#round for aesthetics
+           #figure out what phi A is for graphing
+            words =  re.split('/',cwd)
+            for word in words:
+                if 'phiA0' in word:
+                    self.phi = float(re.sub('[^0-9.]','',word))
+            if not self.phi:
+               raise ValueError('Could not find phi in directory name')
+      
+    def draw_linear(self,ax,colors):
+        '''
+         a method that draws linear polymers in the case that n = 1 
+        '''
+        numsins = 20
+        ax.axis('equal')#need to be equal for proper visual scaling
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+        n =1 
+        #make the polymers extra long, so that the section that is the right arclength can be cut off
+        #this is not the most robust solution but it is quick and probably wont fail
+        x = np.linspace(0,n*5,10000)
+        w = 1.0/(np.random.rand(numsins)*3*n/10 +n/10 )
+        phase_shift = np.random.rand(numsins)*np.pi
+        y = np.zeros(np.shape(x))
+        for w, phase_shift in zip(w,phase_shift):
+            sign = [-1,1][np.random.randint(2)]#Add a random sign for more random appearance
+            y += np.cos(x*w+phase_shift)*sign
+        y = y/np.max(np.abs(y))*x
+        arclength = [0]
+        i=1
+        while arclength[-1] < n:
+             arclength.append(arclength[-1]+np.sqrt((y[i]-y[i-1])**2+(x[i]-x[i-1])**2))
+             i+=1
+        x,y = x[:i],y[:i]
+        plotted = 0
+        arclength = np.array(arclength)
+        for myarm in self.armlist:
+       	     sections = plotted+(1-np.cumsum(myarm.block_frac))*myarm.length#use the block fractions to divide into arclength sections the 1- is  because they are reversed in polyfts
+       	     sections = np.flip(sections,0)
+       	     myarm.species.reverse()
+       	     for i in range(len(sections)):
+       	         if i != len(sections)-1:
+       	             insection = np.logical_and(arclength>=sections[i],arclength<sections[i+1])
+       	         else:
+       	             insection = np.logical_and(arclength>=sections[i],arclength<=plotted+myarm.length)
+       	         self.add_line(x[insection],y[insection],color=colors[myarm.species[i]])
+             plotted+= myarm.length
+
+    def add_line(self,x,y,color):
+        self.lines.append(line(x,y,color))
+    def draw_lines(self,ax):
+       '''
+       a method to draw all the lines in the star at once for normalization
+       '''
+       x_max,y_max = 0,0
+       for myline in self.lines:
+           if max(abs(myline.x)) > x_max: xmax = max(abs(myline.x))
+           if max(abs(myline.y)) > y_max: ymax = max(abs(myline.y))
+       for myline in self.lines:
+           ax.plot(myline.x,myline.y,color=myline.color,lw=4)
+           ax.axis('square')
+           #ax.set_ylim((-ymax,ymax))
+           #ax.set_xlim((-xmax,xmax))
+           ax.set_xticks([])
+           ax.set_yticks([])
+
+
+
+class line:
+    '''
+    a class that holds lines to be drawn later after normalization
+    '''
+    def __init__(self,x,y,color):
+       self.x,self.y,self.color = x,y,color
+
+
 
 class arm:
     def __init__(self,mystar):
@@ -65,12 +143,12 @@ class arm:
         self.angle = []
         self.sectionsize = []
         self.arclength = []
-    def draw_arm(self,ax,angles,sectionsize,colors):
+    def draw_arm(self,angles,sectionsize,colors):
         ''' a function that draws arms, takes an arm as input, as well as the angle to rotate the arm
             a list of colors and the section size that is the angle that the section of the unit circle
             that the arm can take up'''
         numsins = 20
-        ax.axis('equal')#need to be equal for proper visual scaling
+
         n = self.length
         slope = np.tan(sectionsize/2)
         #make the polymers extra long, so that the section that is the right arclength can be cut off
@@ -99,7 +177,7 @@ class arm:
                 yp = x*np.sin(angle)+y*np.cos(angle)
                 intersecting = self.check_intersect(xp,yp)
                 if attempts > 15:
-                    self.redraw(ax,angles,sectionsize,color)
+                    self.redraw(angles,sectionsize,colors)
                     return
                 attempts += 1 
             self.x.append(xp)
@@ -115,7 +193,7 @@ class arm:
                     insection = np.logical_and(arclength>=sections[i],arclength<sections[i+1])
                 else:
                     insection = np.logical_and(arclength>=sections[i],arclength<=self.length)
-                ax.plot(x[insection],y[insection],color=colors[self.species[i]],lw=3)
+                self.star.add_line(x[insection],y[insection],colors[self.species[i]])
 
 
     def check_intersect(self,xp,yp):
@@ -128,9 +206,9 @@ class arm:
                     if np.any(np.logical_and(np.abs(x-myx) < .005,np.abs(y-myy) < .005)):
                         return True 
         return False
-    def redraw(self,ax,angles,sectionsize,color):
+    def redraw(self,angles,sectionsize,colors):
         self.x,self.y,self.arclength= [],[],[]
-        self.draw_arm(angles,sectionsize,color)
+        self.draw_arm(angles,sectionsize,colors)
 
 
 
@@ -148,6 +226,9 @@ class arm:
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--dirs', action='store',required=True, nargs='+',help='list of directories that contain each .out file to be graphed')
+parser.add_argument('-p','--pickle',action='store',default='',help='the name of the filename to save a python pickle of the figure to for editing somwhere else, or sharing interactively')
+parser.add_argument('--xlabel',action='store',default='f_A',help='the label for the x axis use $"label"$ for latex')
+parser.add_argument('--ylabel',action='store',default='n',help='the label for the y axis use $"label"$ for latex')
 args = parser.parse_args()
 
 dirs=args.dirs
@@ -157,9 +238,7 @@ idir=os.getcwd()
 starlist = []
 for mydir in dirs:
     os.chdir(mydir)
-    starlist.append(star(glob.glob('*.out')[0],idir))#create an arm for each .out file (you should only have one in each directory)
-        
-
+    starlist.append(star(glob.glob('*.out')[0],mydir))#create an arm for each .out file (you should only have one in each directory)
     os.chdir(idir)
 colorlist = ['r','b','c','m','k','g','y']
 n_arms = []
@@ -168,9 +247,9 @@ for mystar in starlist:
     n_arms.append(mystar.n_diblock)
     phi.append(mystar.phi)
 n_arms,phi = list(set(n_arms)),list(set(phi))#find unique n and phi
-n_arms.sort()#need to resort since set removed order
+n_arms.sort(reverse=True)#need to resort since set removed orde
 phi.sort()
-f,axlist = plt.subplots(len(n_arms),len(phi))
+fig,axlist = plt.subplots(len(n_arms),len(phi))
 #make dictionaries that relate phi to a specific subplot
 phi_dict = dict([(phi,i) for i, phi in enumerate(phi)])
 n_dict = dict([(n_arms,i) for i,n_arms in enumerate(n_arms)])
@@ -178,18 +257,42 @@ for mystar in starlist:
     #get the subplot to graph on
     ax_index =  [n_dict[mystar.n_diblock],phi_dict[mystar.phi]]
     if len(np.shape(axlist)) == 1:#in case of a 1 dimensional plot
-        ax_index = sum(ax_index)
-        ax = axlist[ax_index]
+        ax = axlist[sum(ax_index)]
+        multiplot = True
+    elif len(np.shape(axlist)) == 0: #for a 0 dim plot
+        ax = axlist
+        multiplot=False
     else:
-        ax = ax_list[ax_index[0],ax_index[1]]
+        ax = axlist[ax_index[0],ax_index[1]]
+        multiplot=True
     
+    #label edge axes
+    if multiplot:
+        if ax_index[1] == 0:
+           ax.set_ylabel(str(mystar.n_diblock),fontsize=22)
+        if ax_index[0] == len(axlist)-1:
+           ax.set_xlabel(str(mystar.phi),fontsize = 22)
+    else:
+       ax.set_xlabel(str(mystar.phi),fontsize = 22)
+       ax.set_ylabel(str(mystar.n_diblock),fontsize=22)
     #FUTURE IMPROVEMENT:
     #this is a hardcoded style for graphing miktoarms, this can be generalized for any star polymer
     #but that is not worthwile at the moment
-    mystar.armlist[0].draw_arm(ax,[-pi],pi/2,colorlist)
-    diblock_spacing = np.linspace(-pi/2,pi/2,mystar.armlist[1].multiplicity+2)[1:-1] #divide the starting angle into multiplicity plus two sections so endpoints can be removed
-    sectionsize = pi/mystar.armlist[1].multiplicity
-    mystar.armlist[1].draw_arm(ax,diblock_spacing,sectionsize,colorlist)
-plt.axis('off')
+    if mystar.n_diblock != 1:
+        mystar.armlist[0].draw_arm([-pi],pi/2,colorlist)
+        diblock_spacing = np.linspace(-pi/2,pi/2,mystar.armlist[1].multiplicity+2)[1:-1] #divide the starting angle into multiplicity plus two sections so endpoints can be removed
+        sectionsize = pi/mystar.armlist[1].multiplicity
+        mystar.armlist[1].draw_arm(diblock_spacing,sectionsize,colorlist)
+    else:
+        mystar.draw_linear(ax,colorlist)
+    mystar.draw_lines(ax)
+if args.xlabel != '':
+    plt.text(0.5, 0.01, r'$\phi_A$', fontsize=30, transform=plt.gcf().transFigure)
+if args.ylabel != '':
+    plt.text(0.001, 0.5, r'$n$', fontsize=30, transform=plt.gcf().transFigure,rotation='vertical')
+plt.tight_layout()
+if args.pickle != '':
+    with open(args.pickle,'wb') as pic:
+        pickle.dump(fig,pic)
 plt.show()
 
